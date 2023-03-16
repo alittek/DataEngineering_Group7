417cac6dbb12ec00aef344faeb713c5cb48e0c50
DhanushkiMapitigama <dhanumapitigama@gmail.com>
2023-03-16 15:57:08 +0000

    added vertical solution

---
+++ vertical-solution/hadoop/datanode/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
+if [ ! -d $datadir ]; then
+  echo "Datanode data directory not found: $datadir"
+  exit 2
+fi
+
+$HADOOP_HOME/bin/hdfs --config /usr/local/hadoop-3.3.4/etc/hadoop datanode
